f8c6e341c72be681562f7f83660f97129462f446
ericgaspar <junk.eg@free.fr>
2021-02-14 18:49:38 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-nodejs_version=14
+nodejs_version=12
 
 #=================================================
 # PERSONAL HELPERS
