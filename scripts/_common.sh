426fe2d99bd71e6d4ad1e7a36d8db36cb739099c
ericgaspar <junk.eg@free.fr>
2021-12-05 12:26:22 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,9 +7,9 @@
 # dependencies used by the app
 pkg_dependencies="graphicsmagick"
 
-nodejs_version=12
+nodejs_version=16
 
-version_commit=f14837be448ff2dc2acfd14493437299453fac3d 
+version_commit=91022aa5da82c15daedf7b3dc81e299722133a89
 
 #=================================================
 # PERSONAL HELPERS
@@ -22,28 +22,3 @@ version_commit=f14837be448ff2dc2acfd14493437299453fac3d
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
-
-
-# Check the architecture
-#
-# example: architecture=$(ynh_detect_arch)
-#
-# usage: ynh_detect_arch
-#
-# Requires YunoHost version 2.2.4 or higher.
-
-ynh_detect_arch(){
-	local architecture
-	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
-		architecture="arm64"
-	elif [ -n "$(uname -m | grep 64)" ]; then
-		architecture="x86-64"
-	elif [ -n "$(uname -m | grep 86)" ]; then
-		architecture="i386"
-	elif [ -n "$(uname -m | grep arm)" ]; then
-		architecture="arm"
-	else
-		architecture="unknown"
-	fi
-	echo $architecture
-}
